7988ca6a0e730774afbab58256d8dbd14446cffc
ShunOmine <shun12baseball@gmail.com>
2019-06-20 22:15:57 +0900

    finish props

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
+// import Bananas from './Bananas'
+// import LotsOfGreeting from './Greeting'
+import BlinkApp from './Blink'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => BlinkApp);
