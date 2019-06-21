996d5d07b5d220148541798b1792f8858f10fba6
ShunOmine <shun12baseball@gmail.com>
2019-06-21 10:43:22 +0900

    styles finish

--- index.js
+++ index.js
@@ -4,9 +4,19 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
+
+// 画像
 // import Bananas from './Bananas'
+
+// props
 // import LotsOfGreeting from './Greeting'
-import BlinkApp from './Blink'
+
+// state
+// import BlinkApp from './Blink'
+
+// stylesheet
+import LotsOfStyles from './Styles'
+
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => BlinkApp);
+AppRegistry.registerComponent(appName, () => LotsOfStyles);
