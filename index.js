7b260c6bcb0a66b172b1e1161f50e45b1d9ea661
ShunOmine <shun12baseball@gmail.com>
2019-06-21 17:24:24 +0900

    finish height and width

--- index.js
+++ index.js
@@ -3,20 +3,26 @@
  */
 
 import {AppRegistry} from 'react-native';
-// import App from './App';
+// import App from './src/App';
 
 // 画像
-// import Bananas from './Bananas'
+// import Bananas from './src/Bananas'
 
 // props
-// import LotsOfGreeting from './Greeting'
+// import LotsOfGreeting from './src/Greeting'
 
 // state
-// import BlinkApp from './Blink'
+// import BlinkApp from './src/Blink'
 
 // stylesheet
-import LotsOfStyles from './Styles'
+// import LotsOfStyles from './src/Styles'
+
+// Height and Width FixedDimensionsBasic
+import FixedDimensionsBasics from './src/FixedDimensionsBasics'
+
+// Height and Width FlexDimensionsBasics (next)
+// import FlexDimensionsBasics from './src/FlexDimensionsBasics'
 
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => LotsOfStyles);
+AppRegistry.registerComponent(appName, () => FixedDimensionsBasics);
